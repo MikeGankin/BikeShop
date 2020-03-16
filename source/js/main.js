003a358d10a7784f625687ffa7110b678e0a3cd2
Михаил <mikhail-gankin@yandex.ru>
2020-03-16 19:03:55 +0300

    1

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,13 @@
 var leftBtn = document.querySelector('.bikes__left-btn');
 var rightBtn = document.querySelector('.bikes__right-btn');
 
-function SliderControl() {
-  leftBtn.addEventListener('click', function(e) {
+function sliderControl() {
+  leftBtn.addEventListener('click', function (e) {
     e.preventDefault();
-  }
+  });
+  rightBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+  });
 }
 
-console.log(SliderControl());
+console.log(sliderControl());
