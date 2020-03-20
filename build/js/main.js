cdbe76ff588309ad84c3f2e192585dab245cbe76
Михаил <mikhail-gankin@yandex.ru>
2020-03-20 15:19:33 +0300

    1

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-$(document).ready(function () {
-  $('.bikes__list').slick();
-});
+function sliderControl() {
+  var slider = document.querySelector('.bikes__list');
+  slider.slick();
+}
+sliderControl();
+
+// $(document).ready(function () {
+//   $('.bikes__list').slick();
+// });
--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,5 @@
 'use strict';
 
-function sliderControl() {
-  var slider = document.querySelector('.bikes__list');
-  slider.slick();
-}
-sliderControl();
-
-// $(document).ready(function () {
-//   $('.bikes__list').slick();
-// });
+$(document).ready(function () {
+  $('.bikes__list').slick();
+});
