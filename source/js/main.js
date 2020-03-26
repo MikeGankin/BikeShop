9d6cd697a4cb2b68bd8db9b11f39fffbe79ea2ba
Михаил <mikhail-gankin@yandex.ru>
2020-03-26 10:41:14 +0300

    1

--- source/js/main.js
+++ source/js/main.js
@@ -2,32 +2,9 @@
 
 $(document).ready(function () {
   $('.bikes__list').slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     infinite: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 320,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
   });
 
   $('.main-header__hamburger').click(function (event) {
